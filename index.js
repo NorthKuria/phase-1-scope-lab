e3b0159d24051518db1dfc45c01af14580f10e8b
northkuria <kuri4martin@gmail.com>
2022-06-09 22:58:43 +0300

    added funcs

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  return;
+}
+
+const leastFavoriteCustomer = "pip";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bo";
+  return;
+}
